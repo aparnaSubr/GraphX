613286b01e2f3037d36ad72225ce16710f356106
Aparna Subramanian <aparna.subr15@gmail.com>
2016-11-29 20:50:07 +0000

    best time spark pagerank

--- src/main/scala/SparkPageRank.scala
+++ src/main/scala/SparkPageRank.scala
@@ -18,13 +18,13 @@ def main(args: Array[String]) {
         .config("spark.executor.memory", "15g")
         .config("spark.executor.cores", "4")
         .config("spark.task.cpus", "1")
-        .config("spark.default.parallelism", "50")
+        .config("spark.default.parallelism", "20")
         .getOrCreate()
         
         val iters = if (args.length > 1) args(1).toInt else 20
         val lines = spark.read.textFile(args(0)).rdd
         val links = lines.map{ s => val parts = s.split("\\s+")
-        (parts(0), parts(1)) }.distinct().groupByKey().repartition(50).cache()
+        (parts(0), parts(1)) }.distinct().groupByKey().repartition(40).cache()
         var ranks = links.mapValues(v => 1.0)
         
         for (i <- 1 to iters) {
@@ -36,6 +36,6 @@ def main(args: Array[String]) {
         
         val output = ranks.collect()
         spark.stop()
-        output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
+        //output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
     }
 }

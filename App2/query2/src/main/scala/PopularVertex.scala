a5b604149479c6f85265c5ca2a8ed533d63d86aa
Aparna Subramanian <aparna.subr15@gmail.com>
2016-12-06 02:21:00 +0000

    submit part 2

---
+++ App2/query2/src/main/scala/PopularVertex.scala
@@ -0,0 +1,73 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+
+object GraphXQuery2 {
+
+def main(args: Array[String]) {
+        
+        val spark = SparkSession
+        .builder
+        .master("spark://10.254.0.83:7077")
+        .appName("PopularVertex")
+        .config("spark.driver.memory", "5g")
+        .config("spark.eventLog.enabled", "true")
+        .config("spark.eventLog.dir", "file:///home/ubuntu/logs/spark")
+        .config("spark.executor.memory", "15g")
+        .config("spark.executor.cores", "4")
+        .config("spark.task.cpus", "1")
+        .config("spark.default.parallelism", "20")
+        .getOrCreate()
+        
+        val fileRDD = spark.sparkContext.wholeTextFiles("hdfs:///user/ubuntu/words")
+        val vertices = fileRDD.map{x =>
+                                   val key = x._1.split("/words/")(1).split(".txt")(0)
+                                   (key, x._2.split("\n"))}
+
+        val wordsRDD = vertices.flatMapValues(x => x).distinct()
+
+        val words = wordsRDD.map{x =>
+                                 (x._2, x._1)}.distinct().groupByKey()
+
+        val pairs  = words.map{x =>
+                         val arr = x._2.toSeq
+                         val combos = for {
+                                            a <- arr
+                                            b <- arr
+                                            if(a != b)
+                                           } yield((a,b))
+                         (x._1, combos)}.flatMapValues(x => x).distinct()
+
+        val edges = pairs.map{x =>
+                              ((x._2._1, x._2._2), x._1)}.groupByKey()
+
+        val defaultVertex = ("default_vertex")
+        val v = vertices.map{x =>
+                             val id = x._1.split("words_")(1).toLong
+                             (id, x._2.size.toLong)}.collect().toSeq
+        val vertexRDD: RDD[(VertexId, Any)] = spark.sparkContext.parallelize(v)
+        val e = edges.map{x =>
+                          Edge(x._1._1.split("words_")(1).toLong, x._1._2.split("words_")(1).toLong, x._2)}.collect().toSeq
+        val edgeRDD: RDD[Edge[Iterable[String]]] = spark.sparkContext.parallelize(e)
+
+        val graph = Graph(vertexRDD, edgeRDD, defaultVertex)
+
+        def max(a: (VertexId, Int), b: (VertexId, Int)): (VertexId, Int) = {
+          if (a._2 > b._2) a else b
+          }
+
+        val maxVertex: (VertexId, Int) = graph.outDegrees.reduce(max)
+        val listOfVertices = graph.outDegrees.filter{x => x._2 == maxVertex._2}
+        val intVertices = graph.vertices.map{x =>
+                                             (x._1, x._2.asInstanceOf[Long])}
+        def maxLong(a: (VertexId, Long), b: (VertexId, Long)): (VertexId, Long) = {
+        if (a._2 > b._2) a else b}
+        val maxWords = intVertices.reduce(maxLong)
+
+        val result = graph.vertices.filter{x => x._2 == maxWords._2}.collect()
+        result.foreach(x => println("--------------------------------------------------------------\nMOST POPULAR VERTEX ID = "
+                                    + x._1 + "\n--------------------------------------------------------------"))
+
+        spark.stop()
+    }
+}
---
+++ part-b/PartBApplication2Question3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+current_dir=`pwd`
+
+cd
+. ~/run.sh
+run_hdfs=`jps | grep -i namenode`
+if [ ${#run_hdfs} == 0 ]; then
+    start_hdfs
+else
+    echo 'HDFS daemons running.'
+fi    
+
+run_spark=`jps | grep -i master`
+if [ ${#run_spark} == 0 ]; then
+    start_spark
+else
+    echo 'Spark daemons running.'
+fi
+
+cd ~/GraphX/App2/query3/
+sbt package
+
+cd
+cache_clean
+
+~/software/spark-2.0.0-bin-hadoop2.6/bin/spark-submit ~/GraphX/App2/query3/target/scala-2.11/graphx-query3_2.11-1.0.jar
+
+trap SIGHUP SIGKILL SIGTERM SIGINT
+
+cd $current_dir
